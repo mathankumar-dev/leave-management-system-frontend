a9913bfb384de23b17ed5a0f398573eb7b3902a2
mathankumar-dev <mathankumardevv@gmail.com>
2026-02-19 13:00:56 +0530

    refact: login

--- src/config/environment.ts
+++ src/config/environment.ts
@@ -2,16 +2,17 @@ export interface EnvironmentConfig {
   API_BASE_URL: string;
   ENDPOINTS: {
     LOGIN: string;
-    REGISTER: string;
   };
 }
 
 export const ENV: EnvironmentConfig = {
   // API_BASE_URL:
   //   import.meta.env.VITE_API_BASE_URL || "http://localhost:5000", 
-  API_BASE_URL : "http://localhost:5000",
+  // API_BASE_URL : "http://localhost:5000/",
+    API_BASE_URL : "https://fqkvs6nm-8080.inc1.devtunnels.ms/",
+
   ENDPOINTS: {
     LOGIN: "/auth/login",
-    REGISTER: "/auth/register",
+    
   },
 };
