9860f12e417af35ce3afea82b84a59e11a5d0a49
Naveen6380 <naveenbarath276@gmail.com>
2026-03-02 18:10:53 +0530

    profile endpoint

--- src/features/auth/services/AuthService.ts
+++ src/features/auth/services/AuthService.ts
@@ -10,7 +10,7 @@ export const authService = {
 
 
 getEmployeeProfile: async (id: number): Promise<User> => {
-    const response = await api.get<User>(`/employees/profile?employeeId=${id}`);
+    const response = await api.get<User>(`/employees/profile/${id}`);
     return response.data;
   }
 };
