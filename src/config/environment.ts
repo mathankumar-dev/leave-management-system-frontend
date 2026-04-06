538d3dad7a522636e35d543c713efd3bbb7ca1f5
Shaly S <shalysurjen@gmail.com>
2026-04-06 16:56:33 +0530

    landing page ui and logo changed

--- src/config/environment.ts
+++ src/config/environment.ts
@@ -4,9 +4,9 @@ export interface EnvironmentConfig {
 }
 
 export const ENV: EnvironmentConfig = {
-  API_BASE_URL: "http://localhost:8111/api",
+  // API_BASE_URL: "http://localhost:8111/api",
   // API_BASE_URL: '/api',
-  // API_BASE_URL: "https://jgpq493j-8111.inc1.devtunnels.ms/api",
+  API_BASE_URL: "https://jgpq493j-8111.inc1.devtunnels.ms/api",
   // API_BASE_URL: "https://fqkvs6nm-8080.inc1.devtunnels.ms/api",
   // API_BASE_URL: "https://fqkvs6nm-8080.inc1.devtunnels.ms/api",
 
