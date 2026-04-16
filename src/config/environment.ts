e70f7ed79172893ad8d16d42fad75588cd7d919f
satheesh <satheesh20046g@gmail.com>
2026-04-16 13:02:03 +0530

    gqv

--- src/config/environment.ts
+++ src/config/environment.ts
@@ -7,8 +7,8 @@ export const ENV: EnvironmentConfig = {
   // API_BASE_URL: "http://localhost:8111/api",
   // API_BASE_URL: "http://192.168.1.14:8111/api",
   // API_BASE_URL: '/api',
-  API_BASE_URL: "https://jgpq493j-8111.inc1.devtunnels.ms/api",
+  // API_BASE_URL: "https://jgpq493j-8111.inc1.devtunnels.ms/api",
   // API_BASE_URL: "https://ithuthandabackendu.loca.lt/api",
-  // API_BASE_URL: "https://fqkvs6nm-8111.inc1.devtunnels.ms/api",
+  API_BASE_URL: "https://fqkvs6nm-8111.inc1.devtunnels.ms/api",
  
 };  
